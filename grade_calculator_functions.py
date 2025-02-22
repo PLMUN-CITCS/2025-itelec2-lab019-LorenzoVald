edaeb97d69d01cb06fafc343a51ab0675398bbae
LorenzoVald <>
2025-02-22 19:01:02 +0800

    Last Sync: 2025-02-22 11:01:02 (Mobile)

---
+++ grade_calculator_functions.py
@@ -0,0 +1,17 @@
+# Get user input
+score = int(input("Enter your score: "))
+
+# Determine the grade
+if score >= 90:
+    grade = "A"
+elif score >= 80:
+    grade = "B"
+elif score >= 70:
+    grade = "C"
+elif score >= 60:
+    grade = "D"
+else:
+    grade = "F"
+
+# Print the result
+print("Your Grade is:", grade)
